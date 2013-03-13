90f32640f3101b07d0c15eb714ca8b4e75e53856
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-03-13 14:09:46 +0000

    Comments, cleanup

--- src/eu/trentorise/smartcampus/ac/UserRegistration.java
+++ src/eu/trentorise/smartcampus/ac/UserRegistration.java
@@ -1,17 +1,31 @@
+/*******************************************************************************
+ * Copyright 2012-2013 Trento RISE
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either   express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package eu.trentorise.smartcampus.ac;
 
-
-import java.io.IOException;
-
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import eu.trentorise.smartcampus.ac.authenticator.AMSCAccessProvider;
 
-
+/**
+ * Dialog for user registration request
+ * @author m.chini
+ *
+ */
 public class UserRegistration {
 	static AlertDialog.Builder builder;
 	public static void upgradeuser(final Context ctx, final Activity activity) {
@@ -30,8 +44,7 @@ public class UserRegistration {
 							switch (which) {
 							case DialogInterface.BUTTON_POSITIVE:
 								//upgrade the user
-								AMSCAccessProvider ac = new AMSCAccessProvider();
-								ac.promote(activity, null, ac.readToken(ctx, null));
+								accessprovider.promote(activity, null, accessprovider.readToken(ctx, null));
 								break;
 
 							case DialogInterface.BUTTON_NEGATIVE:
